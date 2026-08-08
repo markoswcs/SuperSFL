b528a8ee7a6e45e9ee37ec48e4fab05a59569c04
markoswcs <markoswcs@gmail.com>
2026-08-08 11:34:46 -0300

    feat: rename to SFL Pro, new icon, iOS PWA install instructions v115

--- js/ui.js
+++ js/ui.js
@@ -5,11 +5,11 @@ const BUMPKIN_EXP = [0,0,2,22,205,555,1155,2155,3405,5405,7905,10905,14405,18405
  * Todos os componentes visuais: home, farm, market, alerts, settings
  */
 
-import Storage from './storage.js?v=114';
-import { NOTIF_TYPES } from './notifications.js?v=114';
-import { EXPANSION_REQUIREMENTS } from './data/expansions.js?v=114';
-import { t } from './i18n.js?v=114';
-import Farm from './farm.js?v=114';
+import Storage from './storage.js?v=115';
+import { NOTIF_TYPES } from './notifications.js?v=115';
+import { EXPANSION_REQUIREMENTS } from './data/expansions.js?v=115';
+import { t } from './i18n.js?v=115';
+import Farm from './farm.js?v=115';
 
 // duplicate removed
 

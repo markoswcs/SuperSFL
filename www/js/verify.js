a9523619496c6a24868cd2cf54d5f5fbc0b045ba
markoswcs <markoswcs@gmail.com>
2026-09-02 22:08:45 -0300

    fix(ci): fix Prepare WWW Folder step with mkdir -p and untrack www/ directory

--- www/js/verify.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let ui = fs.readFileSync('js/ui.js', 'utf8');
-console.log('Fixed NPC block:', !ui.includes('<img src="${npcImg}"'));
-console.log('Fixed Modal Title:', !ui.includes('<img src="https://raw.githubusercontent.com/sunflower-land/sunflower-land/main/src/assets/composters/compost.png" style="width:24px'));
-console.log('Fixed Modal Img URL:', ui.includes(`const imgUrl = 'https://raw.githubusercontent.com/sunflower-land/sunflower-land/main/src/assets/composters/compost.png';`));
